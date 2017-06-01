61388be830f7248fcfaeba0457af7fb2471e799e
marrow <marrow1203@gmail.com>
2017-06-01 11:59:09 +0800

    mm

--- cycle.cc
+++ cycle.cc
@@ -1,10 +1,33 @@
 #include "sources/munp.h"
+
+// rapidjson
+#include "include/rapidjson/document.h"
+#include "include/rapidjson/writer.h"
+#include "include/rapidjson/reader.h"
+#include "include/rapidjson/stringbuffer.h"
+#include "include/rapidjson/rapidjson.h"
+#include "include/rapidjson/filereadstream.h"
+#include "include/rapidjson/filewritestream.h"
+
+// bcm
+#include "include/bcm/bcm2835.h"
+
 #include <string>
 #include <algorithm>
-#include <assert.h>
+#include <cassert>
+#include <cstdio>
 
 const size_t SLEEP = 10;
 bool cflag = false;
+
+// detect wherether the file in server was exist
+void checkFile(int connfd, std::string name);
+// proform message
+void proformMessage(int connfd, std::string &name);
+// process data of file
+void processDatafile(const std::string &name);
+// control the gpio of raspberry
+void blinkGpio();
 
 void checkFile(int connfd, std::string name)
 {
@@ -24,10 +47,8 @@ void checkFile(int connfd, std::string name)
 void proformMessage(int connfd, std::string &name)
 {
     char buf[MAXLINE];
-//     if (!cflag) {
-        checkFile(connfd, name);
-//         cflag = true;
-//     }
+    checkFile(connfd, name);
+
     int filefd;
     // open file and create it with 0664 if not exist
     if ((filefd = open(name.c_str(), O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR | S_IWGRP | S_IROTH)) == -1) {
@@ -42,16 +63,61 @@ void proformMessage(int connfd, std::string &name)
    
 }
 
+void blinkGpio()
+{
+    if (!bcm2835_init()) {
+        printf("bcm2835 init error\n");
+    }
+    
+    // set the pin to be an output
+    bcm2835_gpio_fsel(RPI_GPIO_P1_11, BCM2835_GPIO_FSEL_OUTP);
+    
+    // turn on
+    bcm2835_gpio_write(RPI_GPIO_P1_11, HIGH);
+    bcm2835_delay(1000);
+    
+    bcm2835_close();
+}
+
+void processDatafile(const std::string &name)
+{
+    using namespace rapidjson;
+    
+    // read file
+    FILE* fp;
+    
+    if ((fp = fopen(name.c_str(), "rb")) == NULL) {
+        err_sys("fopen");
+    }
+    
+    char readBuffer[1<<16];
+    FileReadStream in(fp, readBuffer, sizeof(readBuffer));
+    
+    Document d;
+    d.ParseStream(in);
+    
+    auto pos = d.FindMember("light");
+    Value& vl = d[pos->name];
+    
+    int value = vl.GetInt();
+//     printf("light: %d\n", value);
+    if (value) {
+        blinkGpio();
+    }
+    
+    fclose(fp);
+    
+}
 
 int main(int argc, char* argv[])
 {
     int clientfd;
     sockaddr_in servaddr;
-
-    if (argc != 3) {
-        printf("usage: <IPAddress> <Filename>\n");
-        exit(1);
-    }
+    std::string addr = "104.128.84.142";
+//     if (argc != 3) {
+//         printf("usage: <IPAddress> <Filename>\n");
+//         exit(1);
+//     }
 
 again:
     if ((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -61,41 +127,43 @@ again:
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(SERV_PORT);
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) == 0) {
+    if (inet_pton(AF_INET, addr.c_str(), &servaddr.sin_addr) == 0) {
         err_sys("inet_pton error");
     }
 
-    std::string filename {argv[2]};
+    std::string filename = {argv[1]};
     
 //     while (1) {
-        if (connect(clientfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-            printf("connect error\n");
-            exit(1);
-        }
+    if (connect(clientfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        printf("connect error\n");
+        exit(1);
+    }
         
 //         printf("%d ", PFLAG);
-        int16_t flag = htons(PFLAG);
-        Writen(clientfd, &flag, sizeof(flag));
+    int16_t flag = htons(PFLAG);
+    Writen(clientfd, &flag, sizeof(flag));
         
-        int16_t rflag = 0;
-        Readn(clientfd, &rflag, sizeof(rflag));
-        rflag = ntohs(rflag);
+    int16_t rflag = 0;
+    Readn(clientfd, &rflag, sizeof(rflag));
+    rflag = ntohs(rflag);
 //         printf("%d \n", rflag);
         
-        if (rflag == PFLAG) {
-            printf("success connected to %s\n", argv[1]);
-            printf("receiving file %s\n", argv[1]);
-            proformMessage(clientfd, filename);
-            printf("received file %s\n", argv[1]);
-        }
+    if (rflag == PFLAG) {
+        printf("success connected to %s\n", addr.c_str());
+        printf("receiving file %s\n", addr.c_str());
+        proformMessage(clientfd, filename);
+        printf("received file %s\n", addr.c_str());
+    }
         
-        Close(clientfd);
-        
-        unsigned long s = SLEEP;
-        while ((s = sleep(s)))
-            ;
+    Close(clientfd);
     
-        goto again;
+    processDatafile(filename);
+    
+    unsigned long s = SLEEP*2;
+    while ((s = sleep(s)))
+        ;
+    
+    goto again;
 //     }
 
     return 0;
